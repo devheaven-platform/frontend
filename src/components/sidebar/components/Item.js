25817bea5255c0f6afa044a4358b75101f5a15c9
tomdewildt <t.dewildt@student.fontys.nl>
2019-03-22 23:02:00 +0100

    Add collapsible menu to sidebar

---
+++ src/components/sidebar/components/Item.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Item = ( { to, icon, label } ) => (
+    <li>
+        <NavLink exact to={ to } className="sidebar-item" activeClassName="active">
+            <i className={ `fas fa-${ icon }` } />
+            <span>{ label }</span>
+        </NavLink>
+    </li>
+);
+
+Item.propTypes = {
+    to: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+};
+
+export default Item;
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,13 +3,26 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { bool } from "prop-types";
 
-import Item from "./components";
+import { Item, ItemList } from "./components";
 
 const Sidebar = ( { isCollapsed } ) => (
     <nav className={ isCollapsed ? "sidebar collapsed" : "sidebar" }>
         <ul className="sidebar-content">
             <Item to="/" icon="home" label="Home" />
-            <Item to="/account" icon="user" label="Account" />
+            <ItemList icon="boxes" label="Projects">
+                <Item to="/projects/1" icon="box-open" label="Project 1" />
+                <Item to="/projects/2" icon="box-open" label="Project 2" />
+                <Item to="/projects/3" icon="box-open" label="Project 3" />
+                <Item to="/projects" icon="ellipsis-h" label="All" />
+            </ItemList>
+            <Item to="/invoices" icon="file-invoice-dollar" label="Invoices" />
+            <Item to="/clients" icon="users" label="Clients" />
+            <Item to="/agenda" icon="calendar-alt" label="Agenda" />
+            <Item to="/email" icon="envelope" label="Email" />
+            <ItemList icon="building" label="HR">
+                <Item to="/hours" icon="clock" label="Hours" />
+                <Item to="/personnel" icon="user-friends" label="Personnel" />
+            </ItemList>
         </ul>
     </nav>
 );
