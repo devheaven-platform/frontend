71733a2c20ed2338f91224e9c4ad2d2ac33ffe05
drokii <pedrodossantosmarques@gmail.com>
2019-04-02 12:48:23 +0200

    A

--- src/sagas.js
+++ src/sagas.js
@@ -3,6 +3,7 @@ import { formActionSaga as form } from "redux-form-saga";
 
 // Imported sagas
 import { sagas as app } from "./app/duck";
+import { sagas as projects } from "./pages/projects/duck";
 import { sagas as project } from "./pages/projects/project/duck";
 import { sagas as error } from "./components/error/duck";
 
@@ -11,6 +12,7 @@ const sagas = [
     app(),
     form(),
     projects(),
+    project(),
     error(),
 ];
 
