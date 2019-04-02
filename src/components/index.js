6070036f405691d56054ad8c0f7bd247a98bfc3e
Mario <mario.olislaegers@gmail.com>
2019-04-02 10:45:29 +0200

    Fixed import

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export { default as BoardItem } from "./board-item";
 export { default as Header } from "./header";
 export { default as Sidebar } from "./sidebar";
 export { default as Modal } from "./modal";
-export { default as BoardItem } from "./board-item";
 export {
     Form,
     FormField,
