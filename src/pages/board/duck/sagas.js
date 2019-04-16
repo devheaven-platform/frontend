cac8388e1a5edda38806ea5fca3ecf8f67f82eea
Liwus <liweihu12@gmail.com>
2019-04-16 12:16:10 +0200

    Fix board retrieving error

--- src/pages/board/duck/sagas.js
+++ src/pages/board/duck/sagas.js
@@ -7,7 +7,7 @@ import types from "./types";
 function* getBoard( action ) {
     try {
         const { data } = yield call( Axios.get, `/boards/${ action.payload }` );
-        yield put( { type: types.GET_BOARD_SUCCESS, payload: data } );
+        yield put( { type: types.GET_BOARD_SUCCESS, payload: { board: data } } );
     } catch ( error ) {
         yield put( { type: types.GET_BOARD_ERROR } );
     }
