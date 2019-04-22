653aa348e496f101e8d0fa710f285f9c7a8b758f
tomdewildt <t.dewildt@student.fontys.nl>
2019-04-22 12:41:44 +0200

    Fix health check in production & staging

--- src/app/duck/sagas.js
+++ src/app/duck/sagas.js
@@ -1,15 +1,27 @@
-import { call, put, takeEvery } from "redux-saga/effects";
+import {
+    all,
+    call,
+    put,
+    takeEvery,
+} from "redux-saga/effects";
 import { Axios } from "common/helpers";
 import types from "./types";
 
 function* init() {
     // Config axios
-    if ( process.env.NODE_ENV !== "development" ) {
-        Axios.defaults.baseURL = process.env.REACT_APP_API_ENDPOINT;
+    if ( process.env.NODE_ENV === "development" ) {
+        const { REACT_APP_HEALTH_ENDPOINT, REACT_APP_API_ENDPOINT } = process.env;
 
         // Do health check
         try {
-            yield call( Axios.get, "/api/v1/health/" );
+            yield all( [
+                call( Axios.get, `${ REACT_APP_HEALTH_ENDPOINT }/task-management/health/` ),
+                call( Axios.get, `${ REACT_APP_HEALTH_ENDPOINT }/project-management/health/` ),
+            ] );
+
+            // Set default endpoint
+            Axios.defaults.baseURL = REACT_APP_API_ENDPOINT;
+
             yield put( { type: types.VALIDATE_CONNECTION_SUCCESS } );
         } catch ( error ) {
             yield put( { type: types.VALIDATE_CONNECTION_ERROR, error } );
