768169cdaf016af628462640913140fd5529f7c7
Liwei <liweihu12@gmail.com>
2019-04-26 13:27:19 +0200

    Add delete task (#45)
    * Add delete action types
    
    * Add delete task
    
    * Add delete action types
    
    * Add delete task

--- src/pages/board/duck/types.js
+++ src/pages/board/duck/types.js
@@ -3,5 +3,7 @@ import { Generator } from "common/helpers";
 export default Generator.actionType( [
     "GET_BOARD",
     "CREATE_COLUMN",
+    "DELETE_COLUMN",
     "CREATE_TASK",
+    "DELETE_TASK",
 ] );
