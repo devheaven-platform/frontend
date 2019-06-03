2be8d281186f29f714ceeab8b33d848f9e9c4b46
zielige tijger video <s.vansomeren@student.fontys.nl>
2019-06-03 11:01:59 +0200

    client table

---
+++ src/tables/Clients.js
@@ -0,0 +1,32 @@
+import { CELL_TYPES, FILTER_TYPES } from "./Types";
+
+export default [
+    {
+        label: "Name",
+        key: "name",
+        type: CELL_TYPES.TEXT,
+        filter: FILTER_TYPES.SEARCH,
+        link: {
+            to: "/personnel/__KEY__",
+            key: "id",
+        },
+    },
+    {
+        label: "Description",
+        key: "description",
+        type: CELL_TYPES.TEXT,
+        filter: FILTER_TYPES.SEARCH,
+    },
+    {
+        key: "menu",
+        type: CELL_TYPES.CONTEXT_MENU,
+        icon: "ellipsis-v",
+        actions: [
+            {
+                label: "Remove",
+                key: "remove",
+                icon: "trash",
+            },
+        ],
+    },
+];
